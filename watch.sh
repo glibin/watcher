bcb664b7f1e002328c87f5ebf55a04648785cbad
Vitaly Glibin <glibin.v@gmail.com>
2011-12-06 10:54:09 +0400

    Arguments check

--- watch.sh
+++ watch.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-watchmedo-2.7 shell-command --ignore-patterns="workspace.xml" --command='./exec.py' $1
+if [ $# -ne 1 ]
+then 
+	echo "Usage: `basename $0` <path_to_dir_to_watch>"
+	exit 65
+fi
+
+watchmedo-2.7 shell-command --command='./exec.py' $1
 
